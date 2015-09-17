a9fc5db8edc3d374d2d2f4e09816c1f116f8bd9d
Johannes Würbach <johannes.wuerbach@googlemail.com>
2015-09-17 23:41:50 +0200

    Added functions to manually control sc tunnels
    * Added connect / disconnect to setup and destroy a sauce connect tunnel
    * Fixed error handling

--- test/integration/qunit_test.js
+++ test/integration/qunit_test.js
@@ -1,39 +1,67 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
 var chai = require('chai');
 
 chai.use(require('dirty-chai'));
 var expect = require('chai').expect;
 
 var launcher = require('../../lib/saucelauncher-webdriver');
+var connect = launcher.connect;
+var disconnect = launcher.disconnect;
 var serveQUnit = require('../utils').serveQUnit;
 
 describe('QUnit - Integration', function() {
   this.timeout(120000);
 
-  it('runs qunit tests and reports the result', function(done) {
-    serveQUnit(function(port) {
-      launcher({url: 'http://localhost:' + port}, function(err, result) {
-        expect(err).to.not.exist();
-        expect(result).to.have.deep.property('body.passed', true, 'Marked tests as passed');
-        expect(result).to.have.deep.property('body.custom-data.qunit');
+  var url;
 
-        var qunitResult = result.body['custom-data'].qunit;
-        expect(qunitResult.failed).to.eq(0);
-        expect(qunitResult.passed).to.eq(4);
-        expect(qunitResult.total).to.eq(4);
-        done();
+  beforeEach(function (done) {
+    serveQUnit(function(port) {
+      url = 'http://localhost:' + port;
+      done();
+    });
+  });
+
+  it('runs qunit tests and reports the result', function(done) {
+    launcher({url: url}, function(err, result) {
+      if (err) {
+        return done(err);
+      }
+
+      expect(result).to.have.deep.property('body.passed', true, 'Marked tests as passed');
+      expect(result).to.have.deep.property('body.custom-data.qunit');
+
+      var qunitResult = result.body['custom-data'].qunit;
+      expect(qunitResult.failed).to.eq(0);
+      expect(qunitResult.passed).to.eq(4);
+      expect(qunitResult.total).to.eq(4);
+      done();
+    });
+  });
+
+  it('works when controlling the tunnel manually', function() {
+    var pidFile = 'sc.pid';
+    return connect({
+      pidfile: pidFile,
+      logger: console.log,
+      verbose: true,
+      tunnelIdentifier: 'Manual-' + process.env.TRAVIS_JOB_NUMBER
+    }).then(function () {
+      return launcher({
+        url: url,
+        connect: false,
+        tunnelIdentifierSL: 'Manual-' + process.env.TRAVIS_JOB_NUMBER
       });
+    }).then(function (result) {
+      expect(result).to.have.deep.property('body.passed', true, 'Marked tests as passed');
+      expect(result).to.have.deep.property('body.custom-data.qunit');
+
+      return disconnect(pidFile);
     });
   });
 
   it('fails when specifing a small timeout', function(done) {
-    serveQUnit(function(port) {
-      launcher({url: 'http://localhost:' + port, timeout: 1}, function(err, result) {
-        expect(err).to.be.instanceof(Error);
-        done();
-      });
+    launcher({url: url, timeout: 1}, function(err) {
+      expect(err).to.be.instanceof(Error);
+      done();
     });
   });
 });
